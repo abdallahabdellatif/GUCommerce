818cd54138e07c6119091a488566c0cddfb673fb
khaled 99 elshabrawy <khaled.elshabrawy@hotmail.com>
2019-11-06 21:22:23 +0200

    cmnt

--- proj1.sql
+++ proj1.sql
@@ -80,13 +80,13 @@ END
 
 
 --AS A CUSTOMER
-GO
-CREATE FUNCTION	showProducts() ---a , mksl akmlha ama at2kd en elapproach sah
-RETURNS @records TABLE(serial_number INT,p_name VARCHAR(20),price DECIMAL,rate DECIMAL,color VARCHAR(20),image VARCHAR(20),description VARCHAR(100),category VARCHAR(20))
-AS
-BEGIN 
-RETURN
-END
+--GO
+--CREATE FUNCTION	showProducts() ---a , mksl akmlha ama at2kd en elapproach sah
+--RETURNS @records TABLE(serial_number INT,p_name VARCHAR(20),price DECIMAL,rate DECIMAL,color VARCHAR(20),image VARCHAR(20),description VARCHAR(100),category VARCHAR(20))
+--AS
+--BEGIN 
+--RETURN
+--END
 --b,c zy (a) okhtohom ely fo2 el line da bzbt
 GO
 CREATE PROC AddQuestion --d
@@ -106,7 +106,7 @@ INSERT INTO add_in_cart VALUES (@customername,@serial)
 END
 
 GO
-CREATE PROC addToCart --e2
+CREATE PROC removefromCart --e2
 @customername varchar(20), 
 @serial int
 AS 
@@ -116,4 +116,165 @@ END
 
 GO
 CREATE PROC createWishlist --f
-@customername varchar(20), @name varchar(20)ASBEGIN INSERT INTO Wishlist VALUES(@customername,@name)ENDGOCREATE PROC AddtoWishlist --g1@customername varchar(20), @wishlistname varchar(20), @serial intASBEGIN INSERT INTO containsWP VALUES (@wishlistname,@customername,@serial)ENDGOCREATE PROC AddtoWishlist --g2@customername varchar(20), @wishlistname varchar(20), @serial intASBEGIN DELETE FROM containsWP WHERE wishlist_name=@wishlistname AND customer_username=@customername AND product_sn=@serialEND--h,i zy a,b,c naawGOCREATE FUNCTION calculatepriceOrder(@customername varchar(20)) --j1 , customer is name or username?RETURNS DECIMAL(10,2)ASBEGIN DECLARE @sum DECIMAL (10,2)SELECT @sum=SUM(P.price)FROM add_in_cart ac INNER JOIN Product P ON P.serial_number=ac.product_snWHERE ac.customer_username=@cutomernameRETURN @sumENDGOCREATE PROC emptyCart --j2@customername varchar(20)ASBEGINDELETE FROM add_in_cart WHERE customer_username=@customernameENDGO--CREATE PROC makeOrder --j3 dunno msh ader--j4 bardo zy a,b,c,h,j--start with k from this and the other remaining shit--m3 agmal tahyaty ,shokran--zerbew elnwwwwwww
+@customername varchar(20), @name varchar(20)ASBEGIN INSERT INTO Wishlist VALUES(@customername,@name)ENDGOCREATE PROC AddtoWishlist --g1@customername varchar(20), @wishlistname varchar(20), @serial intASBEGIN INSERT INTO containsWP VALUES (@wishlistname,@customername,@serial)ENDGOCREATE PROC removefromWishlist --g2@customername varchar(20), @wishlistname varchar(20), @serial intASBEGIN DELETE FROM containsWP WHERE wishlist_name=@wishlistname AND customer_username=@customername AND product_sn=@serialEND--h,i zy a,b,c naawGOCREATE FUNCTION calculatepriceOrder(@customername varchar(20)) --j1 , customer is name or username?RETURNS DECIMAL(10,2)ASBEGIN DECLARE @sum DECIMAL (10,2)SELECT @sum=SUM(P.price)FROM add_in_cart ac INNER JOIN Product P ON P.serial_number=ac.product_snWHERE ac.customer_username=@cutomernameRETURN @sumENDGOCREATE PROC emptyCart --j2@customername varchar(20)ASBEGINDELETE FROM add_in_cart WHERE customer_username=@customernameENDGO--CREATE PROC makeOrder --j3 dunno msh ader--j4 bardo zy a,b,c,h,i--start with k from this and the other remaining shit--m3 agmal tahyaty ,shokran--zerbew elnwwwwwww--khaled starts here...CREATE PROC showProducts --aASBEGINSELECT *FROM ProductENDGOCREATE PROC ShowProductsbyPrice --bASBEGINSELECT *FROM ProductORDER BY final_priceENDGOCREATE PROC searchbyname --c@text varchar(20)ASBEGINSELECT *FROM Product WHERE product_name=@textENDGOCREATE PROC showWishlistProduct --h
+@customername varchar(20), @name varchar(20)
+AS 
+BEGIN
+SELECT *
+FROM Wishlist
+WHERE username=@customername AND name=@name
+END
+
+GO
+CREATE PROC  viewMyCart --i
+@customer varchar(20)
+AS 
+BEGIN
+SELECT p.*
+FROM CustomerAddstoCartProduct c INNER JOIN Product p ON c.serial_no=p.serial_number
+WHERE c.customer_name=@customer
+END
+
+GO
+CREATE PROC makeOrder --j3  انها حقا لعينة يا جورج
+@customername varchar(20)
+AS
+BEGIN
+DECLARE @amo DECIMAL(10,2)
+SET @amo=dbo.calculatepriceOrder (@customername)
+
+INSERT INTO Orders(customer_name,total_amount)
+VALUES(@customername,@amo)
+
+DECLARE @orderNO INT
+SELECT @orderNO=MAX(order_no)
+FROM Orders
+
+UPDATE Product
+SET customer_order_id=@orderNO
+FROM Product p INNER JOIN CustomerAddstoCartProduct c ON p.serial_number=c.c.serial_no
+WHERE c.customer_name=@customername
+
+EXEC emptyCart @customername
+END
+
+GO
+CREATE PROC productsinorder --j4
+@customername varchar(20), @orderID int
+AS 
+BEGIN
+SELECT p.*
+FROM Product p INNER JOIN Orders o ON p.customer_order_id=o.order_no
+WHERE o.customer_name=@customername AND o.order_no=@orderID
+END
+
+GO
+CREATE PROC cancelOrder --k
+@orderid int
+AS
+BEGIN
+IF order_status ='not processed' or order_status='in process'
+DELETE FROM Orders
+WHERE order_no=@orderid
+ELSE
+PRINT 'no can dosville baby doll'
+END
+
+GO
+CREATE PROC returnProduct --l
+@serialno int, @orderid int
+AS
+BEGIN
+SELECT *
+FROM Product p INNER JOIN Orders o ON o.order_no=p.customer_order_id
+WHERE p.serial_number=@serialno AND p.customer_order_id=@orderid 
+AND o.order_status='delivered'
+END
+
+GO
+CREATE PROC ShowproductsIbought --m
+@customername varchar(20)
+AS
+BEGIN
+SELECT *
+FROM Product
+WHERE customer_username=@customername
+END
+
+GO
+CREATE PROC rate --n
+@serialno int, @rate int , @customername varchar(20)
+AS
+BEGIN
+UPDATE Product
+SET rate=@rate
+WHERE customer_username=@customername AND serial_number=@serialno
+END
+
+GO
+CREATE PROC SpecifyAmount --o ana habed be nesbet 60%
+@customername varchar(20), @orderID int, @cash decimal(10,2), @credit decimal(10,2)
+AS
+BEGIN 
+IF @cash + @credit <total_amount
+UPDATE Orders
+SET cash_amount=@cash  ,credit_amount=@credit ,payement_type='partially'
+WHERE customer_name=@customername AND order_no=@orderID
+ELSE
+UPDATE Orders
+SET cash_amount=@cash  ,credit_amount=@credit ,payement_type='totally'
+WHERE customer_name=@customername AND order_no=@orderID
+
+UPDATE Customer
+SET points=points+@credit --mesh 3aref ba2a 
+END
+
+GO
+CREATE PROC ChooseCreditCard --p
+@creditcard varchar(20), @orderid int
+AS
+BEGIN
+UPDATE Orders
+SET creditCard_number=@creditcard
+WHERE order_no=@orderid
+END
+
+GO
+CREATE PROC vewDeliveryTypes --q
+AS 
+BEGIN
+SELECT delivery_type --aw fees we duration ma3rafsh
+FROM Delivery
+END
+
+GO
+CREATE PROC specifydeliverytype --r
+@orderID int, @deliveryID INT
+AS
+BEGIN
+UPDATE Orders
+SET delivery_id=@deliveryID
+WHERE order_no=@orderID
+END
+
+GO
+CREATE PROC trackRemainingDays --s
+@orderid int, @customername varchar(20), --el input customername malosh lazma
+@days INT OUTPUT                      --fa homa 7atino leh
+AS                                     --fa 8aleban ely ana 3amlo 8alat
+BEGIN
+SELECT @days=remaining_days
+FROM Orders
+WHERE order_no=@orderid AND customer_name=@customername
+END
+
+GO
+CREATE PROC recommmend --t
+@customername varchar(20) -- mesh ader akamel 
+AS                         --انتظرونا في حلقة قادمة مع برنامج شبراوي للهبد
+BEGIN
+SELECT p.category ,COUNT(p)
+FROM CustomerAddstoCartProduct cap INNER JOIN Product p ON cap.serial_no=p.serial_number
+WHERE cap.customer_name=@customername
+GROUP BY p.category
+END
